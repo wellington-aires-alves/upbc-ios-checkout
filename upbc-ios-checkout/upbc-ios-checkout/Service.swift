12928ce2e9394a5981a588ff995653bcba0d2309
vipx <vipx@outlook.com.br>
2020-04-14 02:59:07 -0300

    finished our checkout

---
+++ upbc-ios-checkout/upbc-ios-checkout/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  upbc-ios-checkout
+//
+//  Created by Wellington on 14/04/2020.
+//  Copyright © 2020 wellington. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    private init(){
+        
+    }
+    public static func doSomething() -> String {
+        return "Did some Stuff, here you go homeboi!"
+    }
+}
---
+++ upbc-ios-checkout/upbc-ios-checkout/upbc_ios_checkout.h
@@ -0,0 +1,19 @@
+//
+//  upbc_ios_checkout.h
+//  upbc-ios-checkout
+//
+//  Created by Wellington on 14/04/2020.
+//  Copyright © 2020 wellington. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for upbc_ios_checkout.
+FOUNDATION_EXPORT double upbc_ios_checkoutVersionNumber;
+
+//! Project version string for upbc_ios_checkout.
+FOUNDATION_EXPORT const unsigned char upbc_ios_checkoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <upbc_ios_checkout/PublicHeader.h>
+
+
